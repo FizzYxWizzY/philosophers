62d165fe8462742ff89c377a7209e988523172f0
FizzYxWizzY <flurymike.1995@gmail.com>
2023-11-15 02:11:09 +0100

    some try to understand

--- philosophers.c
+++ philosophers.c
@@ -6,18 +6,34 @@
 /*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:37:15 by mflury            #+#    #+#             */
-/*   Updated: 2023/11/14 19:09:36 by mflury           ###   ########.fr       */
+/*   Updated: 2023/11/15 02:04:41 by mflury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*routine(void *tid)
+void	*routine(void *mutex)
 {
-	printf("hi, im thread%s! how are you doing folk?\n", (char *)tid);
-	sleep(1);
-	printf("Thread: byebye friend.\n");
-	return NULL;
+	static int	i = 0;
+	
+	while (1)
+	{
+		pthread_mutex_lock((pthread_mutex_t *)mutex);
+		// pthread_mutex_lock((pthread_mutex_t *)mutex);
+		printf("Thread: %d\n", i);
+		i++;
+		usleep(100000);
+		pthread_mutex_unlock((pthread_mutex_t *)mutex);
+		if (i >= 100)
+		{
+			printf("Thread: byebye friend.\n");
+			return NULL;
+		}
+	}
+	// pthread_mutex_unlock((pthread_mutex_t *)mutex);
+	// printf("Thread: byebye friend.\n");
+	// pthread_mutex_unlock((pthread_mutex_t *)mutex);
+	// return NULL;
 }
 
 int	main(int argc, char **argv)
@@ -26,10 +42,13 @@ int	main(int argc, char **argv)
 	(void)argv;
 	pthread_t	t1;
 	pthread_t	t2;
-	
-	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_create(&t2, NULL, &routine, NULL);
+	pthread_mutex_t	mutex;
+
+	pthread_mutex_init(&mutex, NULL);	
+	pthread_create(&t1, NULL, &routine, (void *)&mutex);
+	pthread_create(&t2, NULL, &routine, (void *)&mutex);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
